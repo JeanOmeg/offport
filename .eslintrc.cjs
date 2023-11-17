8f262cbf126df18dc10a53e581babc06f52d2e8e
JeanOmeg <jeanomeg3@gmail.com>
2023-11-16 23:31:49 -0300

    inicializando o repositorio

---
+++ .eslintrc.cjs
@@ -0,0 +1,81 @@
+module.exports = {
+  root: true,
+
+  parserOptions: {
+    parser: require.resolve('@typescript-eslint/parser'),
+    extraFileExtensions: ['.vue'],
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'vue/setup-compiler-macros': true
+  },
+
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-strongly-recommended'
+  ],
+
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-html',
+    'vue',
+    'html'
+  ],
+
+  ignorePatterns: ["jest.config.ts", "database/**/*"],
+
+  globals: {
+    ga: 'readonly',
+    cordova: 'readonly',
+    __statics: 'readonly',
+    __QUASAR_SSR__: 'readonly',
+    __QUASAR_SSR_SERVER__: 'readonly',
+    __QUASAR_SSR_CLIENT__: 'readonly',
+    __QUASAR_SSR_PWA__: 'readonly',
+    process: 'readonly',
+    Capacitor: 'readonly',
+    chrome: 'readonly'
+  },
+
+  rules: {
+    indent: ['error', 2],
+    'vue/html-indent': 'error',
+    'vue/html-quotes': ['error', 'single', { avoidEscape: true }],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 10
+      },
+      multiline: {
+        max: 10
+      }
+    }],
+    'vue/first-attribute-linebreak': ['error', {
+      singleline: 'beside',
+      multiline: 'below'
+    }],
+    'vue/mustache-interpolation-spacing': ['error', 'always'],
+    'vue/component-definition-name-casing': ['error', 'kebab-case'],
+    'prefer-promise-reject-errors': 'off',
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'never'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'space-before-function-paren': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+    'no-undef': 'off',
+    'array-bracket-spacing': 'error',
+    'comma-dangle': 'error',
+    'space-before-blocks': 'error',
+    '@typescript-eslint/prefer-as-const': 'error'
+  }
+}
---
+++ quasar.config.js
@@ -0,0 +1,165 @@
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { configure } = require('quasar/wrappers')
+
+module.exports = configure(function (ctx) {
+  return {
+    supportTS: {
+      tsCheckerConfig: {
+        eslint: {
+          enabled: true,
+          files: './src/**/*.{ts,tsx,js,jsx,vue}'
+        }
+      }
+    },
+
+    boot: [
+      'axios'
+    ],
+
+    sourceFiles: {
+      electronMain: 'src-electron/electron-main.ts',
+      electronPreload: 'src-electron/electron-preload.ts'
+    },
+
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:vue/vue3-strongly-recommended' // Priority B: Strongly Recommended (Improving Readability)
+    ],
+
+    css: [
+      'app.scss'
+    ],
+
+    extras: [
+      'roboto-font',
+      'material-icons'
+    ],
+
+    build: {
+      vueRouterMode: 'hash',
+      env: {
+        API_URL: ctx.dev ? process.env.API_DEV : process.env.API_PROD
+      }
+    },
+
+    devServer: {
+      https: false,
+      port: 9000,
+      open: true
+    },
+
+    framework: {
+      config: {},
+
+      iconSet: 'material-icons',
+      plugins: [
+        'LocalStorage',
+        'Loading',
+        'Notify'
+      ]
+    },
+
+    animations: [],
+
+    ssr: {
+      pwa: true,
+      prodPort: 3000,
+
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      middlewares: [
+        ctx.prod ? 'compression' : '',
+        'render'
+      ]
+    },
+
+    pwa: {
+      workboxPluginMode: 'GenerateSW',
+      workboxOptions: {
+        maximumFileSizeToCacheInBytes: 300000000000000,
+        skipWaiting: true,
+        clientsClaim: true
+      },
+
+      manifest: {
+        name: 'offPort',
+        short_name: 'OP',
+        description: 'Sistema de controle de acesso e gestão de portaria',
+        display: 'standalone',
+        orientation: 'portrait',
+        background_color: '#ffffff',
+        theme_color: '#027be3',
+        icons: [
+          {
+            src: 'icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    },
+
+    cordova: {
+      // noIosLegacyBuildFlag: true,
+    },
+
+    capacitor: {
+      hideSplashscreen: true
+    },
+
+    electron: {
+      bundler: 'packager',
+
+      packager: {
+
+        // OS X / Mac App Store
+        // appBundleId: '',
+        // appCategoryType: '',
+        // osxSign: '',
+        // protocol: 'myapp://path',
+
+        // Windows only
+        // win32metadata: { ... }
+      },
+
+      builder: {
+        // https://www.electron.build/configuration/configuration
+
+        appId: 'off-port'
+      },
+
+      // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
+      chainWebpackMain (/* chain */) {
+        // do something with the Electron main process Webpack cfg
+        // extendWebpackMain also available besides this chainWebpackMain
+      },
+
+      // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
+      chainWebpackPreload (/* chain */) {
+        // do something with the Electron main process Webpack cfg
+        // extendWebpackPreload also available besides this chainWebpackPreload
+      }
+    }
+  }
+})
