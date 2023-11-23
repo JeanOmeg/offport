ba945d4f9622b697ab66cb538d36ec6633f59641
JeanOmeg <jeanomeg3@gmail.com>
2023-11-23 02:53:20 -0300

    ajuste no css

--- src/pages/login-page.vue
+++ src/pages/login-page.vue
@@ -3,10 +3,10 @@
     <div class='col-6'>
       <q-img src='../assets/offPort.jpeg' style='height: 90vh; width: 100%;' />
     </div>
-    <div class='row col-6 justify-center items-center'>
+    <div class='row col-6 justify-center items-center q-mt-xl'>
       <q-form @submit='enviarLogin' class='row col-12 items-center justify-center'>
         <div class='col-12 text-h6 text-center q-mb-md'>
-          <span class='text-h1' style='font-style: italic; font-weight: 500;'>offPort</span><br>Solução completa para seu condomínio
+          <span class='text-h1' style='font-style: italic; font-weight: 500;'>offPort</span><br>Solução completa e offline para seu condomínio
         </div>
         <div class='col-8'>
           <q-input filled v-model='formulario.login' type='text' label='Login' :rules="[ (val) => val && val.length > 0 && val.length < 40 || 'Preencha o campo de login corretamente!']" />
@@ -17,7 +17,7 @@
         <div class='row justify-center col-12 q-mb-md'>
           <q-btn label='Login' color='primary' type='submit' size='md' />
         </div>
-        <div class='text-h6 text-center q-mb-md' style='font-style: italic;'>
+        <div class='text-h6 text-center q-mb-md' style='font-style: italic; font-size: 14px;'>
           Jean Fernandes - www.github.com/JeanOmeg
         </div>
       </q-form>
